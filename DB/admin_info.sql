d8286f186f0e0135b1d791e2655890be0f5f1134
heon0502 <heon2908@gmail.com>
2021-05-03 21:21:31 +0900

    DB 테이블

---
+++ DB/admin_info.sql
@@ -0,0 +1,39 @@
+-- --------------------------------------------------------
+-- 호스트:                          127.0.0.1
+-- 서버 버전:                        10.6.0-MariaDB - mariadb.org binary distribution
+-- 서버 OS:                        Win64
+-- HeidiSQL 버전:                  11.1.0.6116
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- hoseo 데이터베이스 구조 내보내기
+DROP DATABASE IF EXISTS `hoseo`;
+CREATE DATABASE IF NOT EXISTS `hoseo` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `hoseo`;
+
+-- 테이블 hoseo.admin_info 구조 내보내기
+DROP TABLE IF EXISTS `admin_info`;
+CREATE TABLE IF NOT EXISTS `admin_info` (
+  `admin_login` varchar(20) NOT NULL,
+  `group_code` bigint(20) NOT NULL,
+  PRIMARY KEY (`admin_login`),
+  KEY `group_code` (`group_code`),
+  CONSTRAINT `admin_info_ibfk_1` FOREIGN KEY (`group_code`) REFERENCES `group_info` (`group_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 테이블 데이터 hoseo.admin_info:~0 rows (대략적) 내보내기
+DELETE FROM `admin_info`;
+/*!40000 ALTER TABLE `admin_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin_info` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ DB/kiosk_set.sql
@@ -0,0 +1,43 @@
+-- --------------------------------------------------------
+-- 호스트:                          127.0.0.1
+-- 서버 버전:                        10.6.0-MariaDB - mariadb.org binary distribution
+-- 서버 OS:                        Win64
+-- HeidiSQL 버전:                  11.2.0.6213
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- hoseo 데이터베이스 구조 내보내기
+DROP DATABASE IF EXISTS `hoseo`;
+CREATE DATABASE IF NOT EXISTS `hoseo` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `hoseo`;
+
+-- 테이블 hoseo.kiosk_set 구조 내보내기
+DROP TABLE IF EXISTS `kiosk_set`;
+CREATE TABLE IF NOT EXISTS `kiosk_set` (
+  `kiosk_serial_number` varchar(20) NOT NULL,
+  `building_nmae` varchar(20) NOT NULL,
+  `detail_position` varchar(30) NOT NULL,
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL,
+  `group_code` bigint(20) NOT NULL,
+  PRIMARY KEY (`kiosk_serial_number`),
+  KEY `group_code` (`group_code`),
+  CONSTRAINT `kiosk_set_ibfk_1` FOREIGN KEY (`group_code`) REFERENCES `group_info` (`group_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 테이블 데이터 hoseo.kiosk_set:~0 rows (대략적) 내보내기
+DELETE FROM `kiosk_set`;
+/*!40000 ALTER TABLE `kiosk_set` DISABLE KEYS */;
+/*!40000 ALTER TABLE `kiosk_set` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
