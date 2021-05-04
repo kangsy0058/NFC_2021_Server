eba01a65192e790c4e216180344c5b23e907c39f
heon0502 <heon2908@gmail.com>
2021-05-04 18:47:30 +0900

    init.sql 수정

--- DB/maria-compose/maria-init-files/init.sql
+++ DB/maria-compose/maria-init-files/init.sql
@@ -14,12 +14,10 @@
 
 
 -- hoseo 데이터베이스 구조 내보내기
-DROP DATABASE IF EXISTS `hoseo`;
 CREATE DATABASE IF NOT EXISTS `hoseo` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `hoseo`;
 
 -- 테이블 hoseo.admin_info 구조 내보내기
-DROP TABLE IF EXISTS `admin_info`;
 CREATE TABLE IF NOT EXISTS `admin_info` (
   `admin_login` varchar(20) NOT NULL,
   `group_code` bigint(20) NOT NULL,
@@ -33,27 +31,7 @@ DELETE FROM `admin_info`;
 /*!40000 ALTER TABLE `admin_info` DISABLE KEYS */;
 /*!40000 ALTER TABLE `admin_info` ENABLE KEYS */;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- --------------------------------------------------------
--- 호스트:                          127.0.0.1
--- 서버 버전:                        10.6.0-MariaDB - mariadb.org binary distribution
--- 서버 OS:                        Win64
--- HeidiSQL 버전:                  11.1.0.6116
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
 -- 테이블 hoseo.group_info 구조 내보내기
-DROP TABLE IF EXISTS `group_info`;
 CREATE TABLE IF NOT EXISTS `group_info` (
   `group_code` bigint(20) NOT NULL,
   `address` varchar(30) DEFAULT NULL,
@@ -61,33 +39,14 @@ CREATE TABLE IF NOT EXISTS `group_info` (
   PRIMARY KEY (`group_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 테이블 데이터 hoseo.group_info:~1 rows (대략적) 내보내기
+-- 테이블 데이터 hoseo.group_info:~0 rows (대략적) 내보내기
 DELETE FROM `group_info`;
 /*!40000 ALTER TABLE `group_info` DISABLE KEYS */;
 INSERT INTO `group_info` (`group_code`, `address`, `group_name`) VALUES
 	(1, 'test', 'test');
 /*!40000 ALTER TABLE `group_info` ENABLE KEYS */;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- --------------------------------------------------------
--- 호스트:                          127.0.0.1
--- 서버 버전:                        10.6.0-MariaDB - mariadb.org binary distribution
--- 서버 OS:                        Win64
--- HeidiSQL 버전:                  11.2.0.6213
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 -- 테이블 hoseo.kiosk_set 구조 내보내기
-DROP TABLE IF EXISTS `kiosk_set`;
 CREATE TABLE IF NOT EXISTS `kiosk_set` (
   `kiosk_serial_number` varchar(20) NOT NULL,
   `building_nmae` varchar(20) NOT NULL,
@@ -104,24 +63,6 @@ CREATE TABLE IF NOT EXISTS `kiosk_set` (
 DELETE FROM `kiosk_set`;
 /*!40000 ALTER TABLE `kiosk_set` DISABLE KEYS */;
 /*!40000 ALTER TABLE `kiosk_set` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
--- --------------------------------------------------------
--- 호스트:                          127.0.0.1
--- 서버 버전:                        10.6.0-MariaDB - mariadb.org binary distribution
--- 서버 OS:                        Win64
--- HeidiSQL 버전:                  11.2.0.6213
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 -- 테이블 hoseo.user_info 구조 내보내기
 CREATE TABLE IF NOT EXISTS `user_info` (
@@ -141,26 +82,7 @@ DELETE FROM `user_info`;
 /*!40000 ALTER TABLE `user_info` DISABLE KEYS */;
 /*!40000 ALTER TABLE `user_info` ENABLE KEYS */;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
--- --------------------------------------------------------
--- 호스트:                          127.0.0.1
--- 서버 버전:                        10.6.0-MariaDB - mariadb.org binary distribution
--- 서버 OS:                        Win64
--- HeidiSQL 버전:                  11.2.0.6213
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 -- 테이블 hoseo.user_log 구조 내보내기
-DROP TABLE IF EXISTS `user_log`;
 CREATE TABLE IF NOT EXISTS `user_log` (
   `wearable_serial_number` varchar(20) NOT NULL,
   `date` date NOT NULL,
@@ -178,6 +100,6 @@ DELETE FROM `user_log`;
 /*!40000 ALTER TABLE `user_log` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
