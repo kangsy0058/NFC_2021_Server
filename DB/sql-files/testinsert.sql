375bcb7515a840b408d07183f8381ec8dfff1eed
heon0502 <heon2908@gmail.com>
2021-05-16 15:55:44 +0900

    테스트 값 insert 스크립트 파일

---
+++ DB/sql-files/testinsert.sql
@@ -0,0 +1,59 @@
+insert into group_info values (0001,'경기도 화성시 17-1','그룹1');
+insert into group_info values (0002,'경기도 화성시 17-2','그룹2');
+insert into group_info values (0003,'경기도 화성시 17-3','그룹3');
+insert into group_info values (0004,'경기도 화성시 17-4','그룹4');
+insert into group_info values (0005,'경기도 화성시 17-5','그룹5');
+insert into group_info values (0006,'충청남도 아산시 11-1','그룹6');
+insert into group_info values (0007,'충청남도 아산시 11-2','그룹7');
+insert into group_info values (0008,'충청남도 아산시 11-3','그룹8');
+insert into group_info values (0009,'충청남도 아산시 11-4','그룹9');
+insert into group_info values (0010,'충청남도 아산시 11-5','그룹10');
+insert into admin_info values ('login1',0001);
+insert into admin_info values ('login2',0001);
+insert into admin_info values ('login3',0001);
+insert into admin_info values ('login4',0001);
+insert into admin_info values ('login5',0001);
+insert into admin_info values ('login6',0002);
+insert into admin_info values ('login7',0003);
+insert into admin_info values ('login8',0004);
+insert into admin_info values ('login9',0004);
+insert into admin_info values ('login10',0004);
+insert into kiosk_set values ('KSN11111','1공학관','충청남도 아산시 배방읍',36.73639139407394, 127.07420661857469,0006);
+insert into kiosk_set values ('KSN11112','2공학관','충청남도 아산시 배방읍',36.73650928774854, 127.07323878624562,0006);
+insert into kiosk_set values ('KSN11113','조형과학관','충청남도 아산시 배방읍',36.73497511836333, 127.07429681995903,0006);
+insert into kiosk_set values ('KSN11114','3공학관','충청남도 아산시 배방읍',36.73639139407394, 127.07420661857469,0007);
+insert into kiosk_set values ('KSN11115','4공학관','충청남도 아산시 배방읍',36.73639139407395, 127.07420661857467,0007);
+insert into kiosk_set values ('KSN11116','5공학관','충청남도 아산시 배방읍',36.73639139407397, 127.07420661857462,0007);
+insert into kiosk_set values ('KSN11117','6공학관','충청남도 아산시 배방읍',36.73639139407395, 127.07420661857461,0008);
+insert into kiosk_set values ('KSN11118','7공학관','충청남도 아산시 배방읍',36.73639139407392, 127.07420661857462,0008);
+insert into kiosk_set values ('KSN11119','8공학관','충청남도 아산시 배방읍',36.73639139407397, 127.07420661857469,0009);
+insert into kiosk_set values ('KSN11120','9공학관','충청남도 아산시 배방읍',36.73639139407394, 127.07420661857469,0010);
+insert into user_log values ('WSN11111','2021-05-16','03:14:18',36.5,'KSN11111');
+insert into user_log values ('WSN11112','2021-05-16','06:14:18',36.5,'KSN11111');
+insert into user_log values ('WSN11113','2021-05-16','03:14:18',36.5,'KSN11111');
+insert into user_log values ('WSN11114','2021-05-16','03:17:18',37.5,'KSN11111');
+insert into user_log values ('WSN11115','2021-05-16','12:15:12',36.5,'KSN11112');
+insert into user_log values ('WSN11116','2021-05-13','03:12:13',36.5,'KSN11116');
+insert into user_log values ('WSN11117','2021-05-15','25:14:18',35.5,'KSN11112');
+insert into user_log values ('WSN11118','2021-05-14','03:17:18',36.0,'KSN11113');
+insert into user_log values ('WSN11119','2021-05-12','03:44:18',37.0,'KSN11115');
+insert into user_log values ('WSN11120','2021-05-13','08:14:18',36.5,'KSN11114');
+insert into user_info values (null,'aaaa.bbbb.cccc','12가24나','로그인1','T','WSN11111');
+insert into user_info values (null,'abaa.bbbb.cccc','13가34나','로그인2','T','WSN11112');
+insert into user_info values (null,'aaba.bbbb.cccc','14가24나','로그인3','T','WSN11113');
+insert into user_info values (null,'aaab.bbbb.cccc','15가84나','로그인4','T','WSN11114');
+insert into user_info values (null,'aaaa.abbb.cccc','16가64나','로그인5','F','WSN11115');
+insert into user_info values (null,'aaaa.babb.cccc','17가34나','로그인6','T','WSN11116');
+insert into user_info values (null,'aaaa.bbab.cccc','18가34나','로그인7','T','WSN11117');
+insert into user_info values (null,'aaaa.bbba.cccc','19가84나','로그인8','F','WSN11118');
+insert into user_info values (null,'aaaa.bbbb.bccc','10가34나','로그인9','T','WSN11119');
+insert into user_info values (null,'aaaa.bbbb.cbcc','11가34나','로그인10','T','WSN11120');
+
+
+
+
+
+
+
+
+
